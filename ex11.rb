136a2a1b196a494cecf8fe52f39e700eec1a7285
pauline <lee6260lee@yahoo.com>
2016-10-27 16:52:25 -0700

    Goal and work for 10/27
    goal to work on Ruby. did a review up to ex 5 
    did 11 and 12

---
+++ ex11.rb
@@ -0,0 +1,6 @@
+print "How old are you?"
+age = gets.chomp
+print "How tall are you?"
+height = gets.chomp
+print "How much do yo weigh?"
+weight = gets.chomp
---
+++ ex5.rb
@@ -0,0 +1,6 @@
+my_name = 'Pauline'
+my_age = 56 # being honest
+my_height = 60 # inches
+
+puts "Let's talk about #{my_name}."
+puts "She's #{my_height} inches tall."
